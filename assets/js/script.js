eef71dbbbe92f1085609a50c245cbb4fa78407cd
BrianCKWang <brian.ck.wang@gmail.com>
2020-12-22 11:53:12 -0500

    add style change when dragged over

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,6 +184,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if(taskListEl){
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -205,11 +206,20 @@ var dropTaskHandler = function(event) {
     }
 
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+    }
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
